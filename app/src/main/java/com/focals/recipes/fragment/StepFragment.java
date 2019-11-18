2fd6125b98d2f2f76b48b95a7891ed6dccbaf8f6
Abhi Handa <ahanda@ebay.com>
2019-11-17 22:03:32 -0800

    Added ExoPlayer.

--- app/src/main/java/com/focals/recipes/fragment/StepFragment.java
+++ app/src/main/java/com/focals/recipes/fragment/StepFragment.java
@@ -1,6 +1,7 @@
 package com.focals.recipes.fragment;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.focals.recipes.R;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +34,10 @@ public class StepFragment extends Fragment {
 
     @BindView(R.id.tv_stepDesc)
     TextView stepDescriptionTextView;
+
+    @BindView(R.id.playerView)
+    SimpleExoPlayerView playerView;
+    SimpleExoPlayer simpleExoPlayer;
 
     String stepDesc;
 
@@ -39,17 +56,32 @@ public class StepFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_step, container, false);
 
-
         ButterKnife.bind(this, view);
+        Uri sampleUri = Uri.parse("https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffdcf9_9-final-product-brownies/9-final-product-brownies.mp4");
 
+        if (simpleExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            simpleExoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+            playerView.setPlayer(simpleExoPlayer);
 
-        if (stepDesc == null) {
-            stepDesc = getActivity().getIntent().getStringExtra("stepDesc");
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getActivity(), "MyRecipes");
+            MediaSource mediaSource = new ExtractorMediaSource(sampleUri, new DefaultDataSourceFactory(
+                    getActivity(), userAgent), new DefaultExtractorsFactory(), null, null);
+            simpleExoPlayer.prepare(mediaSource);
+            simpleExoPlayer.setPlayWhenReady(true);
         }
 
-        stepDescriptionTextView.setText(stepDesc);
+            if (stepDesc == null) {
+                stepDesc = getActivity().getIntent().getStringExtra("stepDesc");
+            }
 
-        return view;
+            stepDescriptionTextView.setText(stepDesc);
+
+            return view;
+
+
     }
-
 }
