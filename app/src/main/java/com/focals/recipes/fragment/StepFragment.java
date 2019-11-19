ae251dec08b291edc02ec110444ce392f5573097
Abhi Handa <ahanda@ebay.com>
2019-11-19 13:04:27 -0800

    Setting Player Position on rotation.

--- app/src/main/java/com/focals/recipes/fragment/StepFragment.java
+++ app/src/main/java/com/focals/recipes/fragment/StepFragment.java
@@ -1,9 +1,12 @@
 package com.focals.recipes.fragment;
 
 
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,8 +35,6 @@ import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -64,6 +65,7 @@ public class StepFragment extends Fragment implements ExoPlayer.EventListener, V
     MediaSource mediaSource;
     private Recipe currentRecipe;
     private ArrayList<HashMap<String, String>> currentRecipeSteps;
+    private long playerCurrentPosition;
 
 
     StepChangeHandler stepChangeHandler;
@@ -91,7 +93,7 @@ public class StepFragment extends Fragment implements ExoPlayer.EventListener, V
         }
 
         stepDescriptionTextView.setText(stepDesc);
-        initializePlayer(videoUri);
+
 
 
         previousButton.setOnClickListener(this);
@@ -105,11 +107,18 @@ public class StepFragment extends Fragment implements ExoPlayer.EventListener, V
 
         setButtonStatus();
 
+
+        if (savedInstanceState != null && savedInstanceState.containsKey("PlayerCurrentPosition")) {
+            playerCurrentPosition = savedInstanceState.getLong("PlayerCurrentPosition");
+        }
+
+        initializePlayer(videoUri, playerCurrentPosition);
+
         return view;
 
     }
 
-    private void initializePlayer(Uri mediaUri) {
+    private void initializePlayer(Uri mediaUri, long currentPosition) {
 
         if (TextUtils.isEmpty(mediaUri.toString())) {
 
@@ -130,11 +139,18 @@ else {
 
                 // Prepare the MediaSource.
                 prepareMediaSource(mediaUri);
+
+
+                if (currentPosition != 0) {
+                    simpleExoPlayer.seekTo(currentPosition);
+                }
+
+
+                if (getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    setFullScreen();
+                }
             }
-
         }
-
-
     }
 
     private void prepareMediaSource(Uri mediaUri) {
@@ -146,8 +162,16 @@ else {
             mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
                     getActivity(), "MyRecipes"), new DefaultExtractorsFactory(), null, null);
             simpleExoPlayer.prepare(mediaSource);
-            simpleExoPlayer.setPlayWhenReady(true);
+            simpleExoPlayer.setPlayWhenReady(true); //TODO: needed?
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putLong("PlayerCurrentPosition", simpleExoPlayer.getCurrentPosition());
+
     }
 
     @Override
@@ -254,5 +278,21 @@ else {
 
     private void showNoVideoToast() {
         Toast.makeText(getActivity(), "No video is available for this step", Toast.LENGTH_LONG).show();
+    }
+
+    private void setFullScreen() {
+
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) playerView.getLayoutParams();
+        params.width=params.MATCH_PARENT;
+        params.height=params.MATCH_PARENT;
+
+        playerView.setLayoutParams(params);
+
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
