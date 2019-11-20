bb523f63ab2707c4b3a8fac5325b7fb19c8d742f
Abhi Handa <ahanda@ebay.com>
2019-11-20 12:22:11 -0800

    Adding comments.

--- app/src/main/java/com/focals/recipes/activity/IngredientsDetailActivity.java
+++ app/src/main/java/com/focals/recipes/activity/IngredientsDetailActivity.java
@@ -1,20 +1,21 @@
 package com.focals.recipes.activity;
 
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.focals.recipes.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import android.os.Bundle;
-import android.widget.ListView;
 
-import com.focals.recipes.adapter.IngredientsAdapter;
-import com.focals.recipes.R;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+/**
+ * This activity is started only for Phone.
+ * For Tablet, IngredientDetailFragment is created.
+ */
 
 public class IngredientsDetailActivity extends AppCompatActivity {
-
 
     @BindView(R.id.lv_ingredients)
     ListView ingredientsListView;
@@ -22,11 +23,12 @@ public class IngredientsDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_detail_ingredients);
+
+        // Binding views with Butterknife
         ButterKnife.bind(this);
 
+        // Setting ActionBar title
         getSupportActionBar().setTitle(getIntent().getStringExtra("name"));
-
     }
 }
