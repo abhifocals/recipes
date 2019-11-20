7bac392a21b9db16f9aa8e870eed368f0102b43c
Abhi Handa <ahanda@ebay.com>
2019-11-20 12:18:21 -0800

    Adding comments.

--- app/src/main/java/com/focals/recipes/activity/StepActivity.java
+++ app/src/main/java/com/focals/recipes/activity/StepActivity.java
@@ -1,30 +1,38 @@
 package com.focals.recipes.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.focals.recipes.R;
 import com.focals.recipes.utils.RecipeJsonParser;
-import com.google.android.exoplayer2.SimpleExoPlayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * This activity is started only for Phone.
+ * For Tablet, StepFragment is created.
+ */
 
 public class StepActivity extends AppCompatActivity {
 
     @BindView(R.id.tv_stepDesc)
     TextView stepDescriptionTextView;
-    SimpleExoPlayer player;
-    int recipePosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int recipePosition;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step);
+
+        // Binding views with Butterknife
         ButterKnife.bind(this);
 
+        // Getting the recipe position from MainActivity
         Intent intent = getIntent();
         recipePosition = getIntent().getIntExtra("recipePosition", -1);
 
@@ -32,5 +40,4 @@ public class StepActivity extends AppCompatActivity {
         String recipeName = RecipeJsonParser.getRecipes().get(recipePosition).getName();
         getSupportActionBar().setTitle(recipeName);
     }
-
 }
